d1b33bfda57f3aa032339c9bae0edcd17431cbff
connielrs <128835308+ConstanzaLorenaRiveraSanchez@users.noreply.github.com>
2024-06-24 09:13:41 -0400

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx'; // Importar Geolocation desde @ionic-native
+
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -20,6 +22,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     MatInputModule,
     MatNativeDateModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    Geolocation // Agregar Geolocation como proveedor aquí
+  ]
 })
 export class HomePageModule {}
