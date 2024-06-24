ed3ef7044074fc36e239ab24706bf926204154a2
connielrs <128835308+ConstanzaLorenaRiveraSanchez@users.noreply.github.com>
2024-06-24 09:13:45 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: '',
@@ -13,23 +15,26 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'compras',
-    loadChildren: () => import('./compras/compras.module').then( m => m.ComprasPageModule)
+    loadChildren: () => import('./compras/compras.module').then(m => m.ComprasPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'tareas',
-    loadChildren: () => import('./tareas/tareas.module').then( m => m.TareasPageModule)
+    loadChildren: () => import('./tareas/tareas.module').then(m => m.TareasPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'recordatorios',
-    loadChildren: () => import('./recordatorios/recordatorios.module').then( m => m.RecordatoriosPageModule)
+    loadChildren: () => import('./recordatorios/recordatorios.module').then(m => m.RecordatoriosPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule)
   },
 ];
 
